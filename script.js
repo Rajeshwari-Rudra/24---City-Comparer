cde897b87ce1663978ae9b269758750c13f413c7
Rajeshwari_Rudra <rajrudra.raj@gmail.com>
2020-03-02 10:19:14 -0600

    commit

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-console.log('INFO: Starting to read scripts.js');
+let city1 = "Missouri";
+let city2 = "Maryville";
 
-console.log('INFO: Getting access to elements');
+function myFunction() {
 
-function calculate() {
-
-    const total = parseFloat(document.getElementById("total").value);
-    const tip = parseFloat(document.getElementById("tip").value);
-    const result = (total + (total*0.055) + tip) + 0.00;
-    const result2 = parseFloat(result.toFixed(2)); 
-    if(isNaN(result2)){
-        document.getElementById("answer").style.color = "red";
-        answer.innerHTML = "Enter some valid input";
-    }
-    else{
-        document.getElementById("answer").style.color = "blue";
-        answer.innerHTML = `Your total is: $${result2}`;
-    }
+    Math.min(city1.length, city2.length)
    
 
 
